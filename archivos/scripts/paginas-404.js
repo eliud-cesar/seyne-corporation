d889befdccc7b76405543fa5edbe809f403a906a
Cesar Eliud <man2personation@gmail.com>
2020-10-30 22:15:01 -0600

    Implementacion de JS de paginas 404 VERSION 1.3.3
    Se implementa el documento javascript donde se incluyen funciones de las
    paginas de error 404 & en proceso.
    
    Para hacer mas facil el enviar a estas dos paginas los links con
    respectivo razonamiento.
    
    Para que el link de donde demos click vaya a una de estas dos paginas debe incluir lo siguiente:
    	onclick="javascript:NOMBRE DE LA FUNCION ();".
    
    	-Esto debe ir en el HTML
    
    El nombre de la funcion se llama
    
    -en proceso = "enProceso"
    -error 404 = "error404"
    
    Se tiene a futuro el refactorizar esta funcion para que con solo una
    clase se pueda realizar esta misma funcion.
    
    No se ha implementado o incluido a todo el codigo.

---
+++ archivos/scripts/paginas-404.js
@@ -0,0 +1,17 @@
+
+
+
+const error404 = () => {
+    location.href = "./archivos/html's/errror-404.html"
+}
+
+
+// onclick="javascript:error404();"
+
+
+const enProceso = () => {
+    location.href = "./archivos/html's/en-proceso.html"
+}
+
+
+// onclick="javascript:enProceso();"
